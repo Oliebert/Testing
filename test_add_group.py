c96be3fd5fa73d9b8f8c52492a6ae3118adc2cfd
olga liebert <olga.liebert@gmail.com>
2016-10-30 18:00:54 +0100

    refractoring by changing signature

--- test_add_group.py
+++ test_add_group.py
@@ -21,9 +21,9 @@ class test_add_group(unittest.TestCase):
         success = True
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.group_creation(wd)
+        self.group_creation(wd, name="knlnölnlmlml", header="GUBJHB", footer="lknlknkln")
         self.return_to_group(wd)
         self.logout(wd)
         self.assertTrue(success)
@@ -34,33 +34,33 @@ class test_add_group(unittest.TestCase):
     def return_to_group(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def group_creation(self, wd):
+    def group_creation(self, wd, name, header, footer):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("knlnölnlmlml")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("ömölmlkm")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("lknlknkln")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
         wd.find_element_by_xpath("//div[@id='content']//form[normalize-space(.)='']").click()
 
