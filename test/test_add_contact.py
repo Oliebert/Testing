7b304446edb5d8757b960b9d78bc3441a60e4b81
olga liebert <olga.liebert@gmail.com>
2016-11-11 14:26:20 +0100

    fixture in test_add_contact has been deleted

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -6,11 +6,6 @@ from fixture.application import Application
 from model.contact import Contact
 
 
-@pytest.fixture
-def app(request):
-    fixture=Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 
