d0d94b1995dee2ee945a27bea8be20e9429e3795
olga liebert <olga.liebert@gmail.com>
2016-12-02 18:51:53 +0100

    Browser options added_2

--- fixture/navigation.py
+++ fixture/navigation.py
@@ -5,16 +5,9 @@ class NavigationHelper:
     def __init__(self, app, browser, base_url):
         self.app = app
         self.base_url = base_url
-        if browser == "firefox":
-            self.wd = webdriver.Firefox()  # firefox_binary="C:\\Program Files\\Mozilla Firefox\\firefox.exe"
-        elif browser == "chrome":
-            self.wd = webdriver.Chrome()
-        elif browser == "ie":
-            self.wd = webdriver.Ie()
-        else:
-            raise ValueError("Unrecognized browser %s" % browser)
 
-    def open_home_page(self):
+
+    def open_home_page(self ):
         wd = self.app.wd
         wd.get(self.base_url)
 
